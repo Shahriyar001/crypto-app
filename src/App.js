b888717454013eba24e671466d9b576eec989a37
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-10-06 23:28:39 +0600

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Switch, Router, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
+// import Navbar from "./components/Navbar";
+import { Navbar } from "./components";
+import "./App.css";
+// import { Switch, Router, Link } from "react-router-dom";
+// import { Layout, Typography, Space } from "antd";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <h1>main</h1>
+      </div>
       <div className="footer"></div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -8,9 +8,14 @@
 
 import React from "react";
 import { createRoot } from "react-dom/client"; // Correct import
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App"; // Make sure this points to the correct App file
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement); // Create the root
-root.render(<App />); // Render the App
+root.render(
+  <Router>
+    <App />
+  </Router>
+); // Render the App
