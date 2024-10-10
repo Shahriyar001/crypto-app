0ba7a7b100105d44802bf43bff488712266f247b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-10-10 22:45:16 +0600

    added api key and url to get data

--- src/services/cryptoAPi.js
+++ src/services/cryptoAPi.js
@@ -1,10 +1,20 @@
-// const options = {
-// 	method: 'GET',
-// 	hostname: 'coinranking1.p.rapidapi.com',
-// 	port: null,
-// 	path: '/stats?referenceCurrencyUuid=yhjMzLPhuIDl',
-// 	headers: {
-// 		'x-rapidapi-key': '67f86cf385msh2f4feadd7f3b554p18ac50jsn454fc3e201de',
-// 		'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-// 	}
-// };
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-key": "67f86cf385msh2f4feadd7f3b554p18ac50jsn454fc3e201de",
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
